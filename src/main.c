f7ed869119918d1ce8cd25ae0621e18eacd80b3d
Kimdamitung <duytungdz3909@gmail.com>
2026-01-12 14:05:30 +0700

    init project

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define F_CPU 11095200UL
+#include <util/delay.h>
+#include <avr/io.h>	
+
+int main(int argc, char const *argv[]){
+	/* code */
+	DDRD |= (1 << DD6);
+	// DDRB |== 
+	while (1) {
+		PORTD |= (1 << DD6);
+		_delay_ms(1000);
+		PORTD &= ~(1 << DD6);
+		_delay_ms(1000);
+	}
+	return 0;
+}
